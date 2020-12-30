2d6cc974890eed245b60682601e03cbc505adec0
allen <allen_abc@yahoo.com>
2020-12-29 21:49:16 -0700

    sfsdf

--- public/client.js
+++ public/client.js
@@ -1,7 +1,7 @@
 // client-side js
 // run by the browser each time your view template referencing it is loaded
 
-console.log("xxx googbye world :o"); 
+console.log("123 googbye world :o"); 
 
 const dreams = [];
 
